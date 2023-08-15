dccb4f657c8a22200edbabdf13a490368136fcf9
B Nagaraj Nitin <139379259+BNagarajNitin@users.noreply.github.com>
2023-08-15 17:52:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Digitalwatch from './Components/Digitalwatch';
+
+function App() {
+  return (
+    <React.Fragment>
+
+<Digitalwatch/>
+    </React.Fragment>
+  );
+}
+
+export default App;
